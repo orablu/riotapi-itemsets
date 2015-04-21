69676bee8ec19a25bf7c8f462f65dae9bee2dcf0
Wes Rupert <ora@outlook.com>
2015-04-20 22:46:49 -0700

    Add view backend for summoner/match views

--- itemviewer/views.py
+++ itemviewer/views.py
@@ -1,19 +1,28 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.template import RequestContext, loader
 from django.http import HttpResponse
 
 from models import Summoner
 
 def index(request):
     summoner_list = Summoner.objects.order_by('name')[:5]
-    template = loader.get_template('itemviewer/index.html')
     context = RequestContext(request, {
         'summoner_list': summoner_list,
     })
-    return HttpResponse(template.render(context))
+    return render(request, 'itemviewer/index.html', context)
 
 def summoner(request, summoner_id):
-    return HttpResponse('At item viewer summoner profile!')
+	try:
+		summoner = Summoner.objects.get(pk=summoner_id)
+	except Summoner.DoesNotExist:
+	    return Http404('Summoner {summoner_id} does not exist.'.format(
+	    	summoner_id=summoner_id
+    	))
 
 def match(request, match_id):
-    return HttpResponse('At item viewer match profile!')
+	try:
+		match = Match.objects.get(pk=match_id)
+	except Match.DoesNotExist:
+	    return Http404('Match {match_id} does not exist.'.format(
+	    	match_id=match_id
+    	))
